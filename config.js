95cb6e3aa8c530a951094b541bc046ed2ce3f7e2
ramzznotdev <ramzznotdev@gmail.com>
2026-04-03 17:48:27 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pakasir: {
+    apikey: "fouP2tIUrNFdQfRrMw2zkMOf0FWp38Lt",
+    project: "auto-order-by-ramzz-official",
+    mode: "api"
+  },
+  waNumber: "6283862592489",
+  adminKey: "RAMZZSUPREME2046",
+  jwtSecret: "ramzz-super-secret-2046",
+  port: 3000
+};
